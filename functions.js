c0e0b4100ba5b4db1666b758ae56d4d39cedfd46
vkrice <78564871+vkrice@users.noreply.github.com>
2021-08-12 17:10:17 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -6,6 +6,7 @@ function initApp(O) {
        LogUserInfo('initApp');
        db.doc('/users/'+email).get().then((doc)=>{
          roles=doc.data().roles?doc.data().roles:['student'];  //admin= (roles.indexOf('instructor')>=0)?1:0;
+         role = (email=='vkumar@utep.edu')?'instructor':'student'; 
         })
      } else { 
        LoginDisplay({id:'Login'}); 
